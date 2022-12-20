7f8cd968948fe795df5f6658bb99c6910992673f
LucasMonastirsky <lucas.mnsk@gmail.com>
2022-12-20 13:43:30 -0300

    smarter spinning

--- src/components/DurationPicker/components/NumberPicker.tsx
+++ src/components/DurationPicker/components/NumberPicker.tsx
@@ -1,27 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { GestureResponderEvent, View, ViewProps } from "react-native"
 import { AppText } from '@src/components'
-import { addLeadingZero } from "@src/utils/helpers"
+import { addLeadingZero, useAsyncState } from "@src/utils/helpers"
 import { SlaveText } from './components'
 import DEBUG from '@src/utils/debug'
 
+type TouchStatus = 'inactive'|'active'|'recovering'
 type NumberPickerProps = { initial_value?: number }
 const NumberPicker = ({ initial_value }: NumberPickerProps) => {
   const [value, setValue] = useState(initial_value ?? 1)
+  const [pos, setPos] = useState(initial_value ?? 0)
 
-  const [touching, setTouching] = useState(false)
-  const [view_height, setViewHeight] = useState(0)
-  const [touch_start, setTouchStart] = useState(0)
-  const [touch_move, setTouchMove] = useState(0)
+  const [touch_status, setTouchStatus] = useState<TouchStatus>('inactive')
+  const [view_height, setViewHeight, getViewHeight] = useAsyncState(0)
+  const [last_touch, setLastTouch, getLastTouch] = useAsyncState(0)
 
-  const touch_delta = touch_start - touch_move
-  const module = touch_delta % view_height
-  const steps = touch_delta / view_height
-  const step_module = Math.trunc(steps) % 3
-  const cycle_count = Math.trunc(steps / 3)
-
-  // note: cycle needs to be relative to starting position and count from there
-  // i don't think it can be done via just math
+  const pos_mod = Math.round(pos % 3)
+  const pos_cycle = Math.trunc(pos / 3)
 
   const container_props: ViewProps = {
     onLayout: (e) => {
@@ -30,33 +25,41 @@ const NumberPicker = ({ initial_value }: NumberPickerProps) => {
     onStartShouldSetResponder: () => true,
     onResponderGrant: ({ nativeEvent }) => {
       DEBUG.log('touch start', 'touch')
-      setTouching(true)
-      setTouchStart(nativeEvent.pageY)
+      setLastTouch(nativeEvent.pageY)
+      setTouchStatus('active')
     },
-    onResponderMove: (evt: GestureResponderEvent) => {
-      DEBUG.log(`${steps.toFixed(1)} | ${step_module} | ${cycle_count}`, 'touch')
-      setTouchMove(evt.nativeEvent.pageY)
+    onResponderMove: ({ nativeEvent: { pageY }}: GestureResponderEvent) => {
+      const delta = last_touch - pageY
+      setPos(pos => pos + delta / view_height)
+      setLastTouch(pageY)
     },
     onResponderRelease: () => {
-      DEBUG.log('touch end', 'touch')
-      setTouching(false)
+      // DEBUG.log(`touch end with ${steps} steps`, 'touch')
+      setTouchStatus('inactive')
+      // setValue(x => x + steps)
     }
   }
 
+  DEBUG.log(`${pos.toFixed(1)} | ${pos_mod.toFixed(1)} | ${pos_cycle.toFixed(1)}`, 'touch')
+
   const text_items = [
-    { offset: -50, value: 0 },
-    { offset: 0 , value: 1 },
-    { offset: 50, value: 2 },
-  ].map((x, i) => ({
-    offset: -50 + 50 * i + (step_module > i ? 150 : 0) + 150 * cycle_count,
-    value: i,
-  }))
+    { offset: 0, value: 0 },
+    { offset: 50 , value: 1 },
+    { offset: 100, value: 2 },
+  ].map((x, i) => {
+    const magic = Math.trunc((Math.round(pos) + 1.5 - i) / 3)
+    const offset = (i * 50) - (pos * 50) + (magic * 150)
+
+    const debug = `${magic}|${Math.trunc(offset)}`
+
+    return { offset, value: i + magic * 3, debug }
+  })
 
   return (
-    <View style={{overflow: 'hidden'}} {...container_props}>
+    <View style={{overflow: 'visible', borderWidth: 1, borderColor: 'gray'}} {...container_props}>
       <AppText size='m' style={{ opacity: 0 }}>{addLeadingZero(value)}</AppText>
-      {text_items.map(x =>
-        <SlaveText offset={x.offset - touch_delta}>{addLeadingZero(x.value)}</SlaveText>
+      {text_items.map((x, i) =>
+        <SlaveText offset={x.offset} debug={x.debug} key={i}>{addLeadingZero(x.value)}</SlaveText>
       )}
     </View>
   )
--- src/components/DurationPicker/components/components/SlaveText.tsx
+++ src/components/DurationPicker/components/components/SlaveText.tsx
@@ -1,13 +1,14 @@
-import { TextStyle } from "react-native"
+import { View, TextStyle, Text } from "react-native"
 import { AppText } from '@src/components'
 
-type SlaveTextProps = { children: string, offset: number }
-const SlaveText = ({ children, offset }: SlaveTextProps) => {
+type SlaveTextProps = { children: string, offset: number, debug?: string }
+const SlaveText = ({ children, offset, debug }: SlaveTextProps) => {
   const style: TextStyle = { position: 'absolute', top: offset + '%', opacity: 0.5 }
 
-  return (
+  return <>
     <AppText {...{style}}>{children}</AppText>
-  )
+    <Text style={{position: 'absolute', top: offset + '%', left: '75%'}}>{debug}</Text>
+  </>
 }
 
 export default SlaveText
